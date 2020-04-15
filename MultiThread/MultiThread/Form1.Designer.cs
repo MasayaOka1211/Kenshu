3cede41c04ccc1fd6c0b7b1c38e35a8f17d1895d
MasayaOka1211 <63706291+MasayaOka1211@users.noreply.github.com>
2020-04-15 19:01:52 +0900

    Add files via upload

---
+++ MultiThread/MultiThread/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace MultiThread
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(48, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(50, 132);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(50, 198);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(184, 198);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 3;
+            this.btnReset.Text = "リセット";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(174, 132);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "label3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ MultiThread/MultiThread/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+
+namespace MultiThread
+{
+    public partial class Form1 : Form
+    {
+        Stopwatch myStopWatch = new Stopwatch();
+
+        //スタート・ストップボタン用
+        Boolean sw = false;
+
+        // 非同期実行するためのデリゲート
+        delegate void SampleDelegate(string filePath, string WriteValue);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Text = DateTime.Now.ToLongTimeString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (sw == false)
+            {
+
+                //計測開始
+
+                myStopWatch.Start();
+
+                //表示更新タイマー開始
+
+                timer2.Start();
+
+                //スイッチon
+
+                sw = true;
+
+                //リセットボタン使用不可
+
+                btnReset.Enabled = false;
+
+                //「スタート」だったボタンの表示を「ストップ」に変更
+
+                btnStart.Text = "ストップ";
+
+            }
+          //スイッチがoff以外のとき（つまりはonのとき
+            else
+            {
+                // 実行するデリゲートを作成
+                SampleDelegate sampleDelegate =
+                    new SampleDelegate(this.DelegatingMethod);
+
+                //計測終了
+
+                //myStopWatch.Stop();
+
+                //表示固定
+
+                //timer2.Stop();
+
+                label3.Text = myStopWatch.Elapsed.ToString();
+
+                 // 非同期で呼び出す
+                IAsyncResult ar =
+                    sampleDelegate.BeginInvoke(@"C:\Test\TimeRecord.txt", label3.Text + System.Environment.NewLine 
+                    , null, null);
+
+                //スイッチoff
+
+                sw = false;
+
+                //リセットボタン使用可
+
+                btnReset.Enabled = true;
+
+                //「ストップ」だったボタンの表示を「スタート」に変更
+
+                btnStart.Text = "スタート";
+
+            }            
+
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            //ストップウォッチの内容をゼロにする
+
+            myStopWatch.Reset();
+
+            //リセットした状態をlabel2に表示する
+
+            label2.Text = myStopWatch.Elapsed.ToString();
+            label3.Text = myStopWatch.Elapsed.ToString();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            label2.Text = myStopWatch.Elapsed.ToString();
+        }
+
+        private void DelegatingMethod(string filePath, string writeValue)
+        {
+            using(StreamWriter sWrite = new StreamWriter(filePath, true, Encoding.UTF8))
+            {
+                sWrite.Write(writeValue);
+            }
+        }
+
+    }
+}
